42dd64cb4cf676a6bfdda801b265e2b39492ea68
Bijay Jha <bijayjha12345@gmail.com>
2025-10-26 15:24:04 +0545

    Constants to work on

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const signUpForm = document.getElementById("signUpForm");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const viewPassword = document.getElementById("viewPassword");
+const submitButton = document.getElementById("submitButton");
+const inputs = signUpForm.querySelectorAll("input");
