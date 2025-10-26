86a360baa85fe0f5dd6e4c740368d67f955e950f
Bijay Jha <bijayjha12345@gmail.com>
2025-10-26 16:10:07 +0545

    function to validateName using regular expressions

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const confirmPasswordInput = document.getElementById("confirmPassword");
 const viewPassword = document.getElementById("viewPassword");
 const submitButton = document.getElementById("submitButton");
 const inputs = signUpForm.querySelectorAll("input");
+
+function validateName{
+    const name = nameInput.value.trim();
+    const regex = /^[A-Za-z\s]+$/;
+    if(name===""){
+        setError(nameInput, "Full name is required");
+        return false;
+    }
+    else if(!regex.test(name)){
+        setError(nameInput, "Name can only contain letters");
+        return false;
+    }
+    else{
+        setSuccess(nameInput);
+        return true;
+    }
+}
